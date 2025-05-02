b95cbd594624c9380fb83460104c2781dfd4cfcd
wnd <22222w@duck.com>
2025-05-02 18:06:07 +1200

    Update bycyc2.js

--- bycyc2.js
+++ bycyc2.js
@@ -1,1 +1,26 @@
+const images = [
+    "/bycyc Evidence/gay/gay1.png",
+    "/bycyc Evidence/gay/gay2.png",
+    "/bycyc Evidence/gay/gay3.png",
+    "/bycyc Evidence/gay/gay4.png",
+    "/bycyc Evidence/gay/gay5.png",
+    "/bycyc Evidence/gay/gay6.png",
+    "/bycyc Evidence/gay/gay7.png",
+    "/bycyc Evidence/gay/gay8.png",
+    "/bycyc Evidence/gay/gay9.png",
+    "/bycyc Evidence/gay/gay10.png",
+    "/bycyc Evidence/gay/gay11.png",
+    "/bycyc Evidence/gay/gay12.png",
+    "/bycyc Evidence/gay/gay13.png",
+    "/bycyc Evidence/gay/gay14.png",
+    "/bycyc Evidence/gay/gay15.png"
+];
+let currentIndex = 0;
 
+function shuffleImage() {
+    document.getElementById('displayedImage').src = images[currentIndex];
+    currentIndex = (currentIndex + 1) % images.length; // Increment index and reset to 0 if it exceeds the array length
+}
+
+shuffleImage();
+setInterval(shuffleImage, 1500);
